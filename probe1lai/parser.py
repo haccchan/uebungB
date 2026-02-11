fbd6e9f6278c2f56fb849ce1862df99f22409fd9
haccchan~ <ptht0711@gmail.com>
2026-02-11 16:20:36 +0100

    .

--- probe1lai/parser.py
+++ probe1lai/parser.py
@@ -1,4 +1,4 @@
-# chuyen str sang tuple
+#4.1 : chuyển str "12 kg" thành tuple ( float 12.0, "kg")
 def parse_weight(str_input: str) -> tuple[float, str]:
     try:
         value_str, unit = str_input.split()
@@ -9,18 +9,18 @@ def parse_weight(str_input: str) -> tuple[float, str]:
 
         return value, unit
 
-    except (ValueError, IndexError, TypeError):
-        print("Invalid input")
+    except Exception as e:
+        print(e)
         return 1.0, str_input
 
-
+#4.2 đổi đơn vị : từ "12 kg","g" thành 12000 , "g"
 def normalize(start_weight: tuple[float, str], target_unit: str) -> tuple[float, str]:
     factors = {"mg": 1, "g": 1000, "kg": 1000000}
     value, unit = start_weight
     new_value = value * factors[unit] / factors[target_unit]
     return new_value, target_unit
 
-# cong thanh einheit trc
+# 4.3 cộng hai cân nặng vào : ("12kg","13000g") thành 25, "kg"
 def add(weight1: str, weight2: str) -> tuple[float, str]:
     w1 = parse_weight(weight1)
     w2 = parse_weight(weight2)
--- probe1lai/rekursion.py
+++ probe1lai/rekursion.py
@@ -12,10 +12,16 @@ def recursive_solution(x: int, z: int = 0) -> int:
 def iterative_solution(x: int) -> int:
     s = 0
     z = 0
+#cách 1
+    for i in range(1,x+1):
+        s = s + i
+        z += 1
+#cách 2
     while x >= 0:
         s = s + x
         x = x - 1
-        z += 1
+        z +=1
+#
     print("schritt i:",z)
     return s
 #s
